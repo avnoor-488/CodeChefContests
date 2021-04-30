8141edf08d37adf5c6e55a17a3147aee5ad996a7
Avnoor singh <54592053+avnoor-488@users.noreply.github.com>
2021-05-01 03:55:06 +0530

    April Lunchtime 2021

---
+++ ArrayRotation.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int i=0;i<n;i++)
+#define MOD 1000000007
+
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long ll;
+
+
+
+void fastscan( ll &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+
+
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll n, count = 0;
+    cin >> n;
+    ll sum = 0;
+    forN(n)
+    {
+        ll q;
+        cin >> q;
+        // cout << q;
+
+        sum = (sum + q + MOD) % MOD;
+    }
+    // cout << sum << endl;
+    ll x;
+    cin >> x;
+    while (x--)
+    {
+        sum = (2 * sum) % MOD;
+        cout << sum << endl;
+    }
+
+}
+
+
+// coded with the ❤️ Avnoor;
