72b576193ffee3998264e3b474ed1f7672707213
Avnoor singh <54592053+avnoor-488@users.noreply.github.com>
2021-05-24 22:52:18 +0530

    May Long Long Challenge 2021

---
+++ Golf.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 1000000007
+
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long ll;
+typedef vector<ll> vi;
+
+
+void fastscan( ll &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+
+
+
+
+int main() {
+
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll t;
+    cin >> t;
+    forT(t)
+    {
+        ll n , x, k;
+        cin >> n >> x >> k;
+
+
+        if ((x % k == 0) or (n + 1 - x) % k == 0 )cout << "yes" << endl;
+        else cout << "no" << endl;
+
+    }
+}
+
+
+// coded with the ❤️ Avnoor;
