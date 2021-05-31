4689d97cf22bb736f812a9e990bd4cfc0bcb82a5
Avnoor singh <54592053+avnoor-488@users.noreply.github.com>
2021-05-31 23:31:55 +0530

    May Lunctime 2021

---
+++ TomAndJerry1.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 1000000007
+#define num 4e6 + 5
+
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vv;
+
+
+void fastscan( ll &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+ll gcd (ll a, ll b)
+{
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t;
+    cin >> t;
+       while(t--)
+    {
+        ll a,b,c,d,k;
+        cin >>a>>b>>c>>d>>k;
+        ll x=abs(c-a);
+        ll y=abs(d-b);
+        ll z=x+y;
+
+        if(k==z)cout << "YES" << endl;
+        if(z>k)cout << "NO" << endl;       
+        if((k-z)%2==1 && k>z)cout << "NO" << endl;
+        if((k-z)%2==0 && k>z)cout << "YES" << endl;       
+        
+    }
+
+    return 0;
+
+}
+
+// coded with the ❤️ Avnoor
+
