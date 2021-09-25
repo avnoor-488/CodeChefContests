c97f5b833d59a17650a2a1ff293f5d572ae7fac9
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2021-09-25 21:37:52 +0530

    Create Yet_another_SOD_problem.cpp

---
+++ September Lunchtime 2021/Yet_another_SOD_problem.cpp
@@ -0,0 +1,102 @@
+// Ujan is a software developer. He is developing a software that takes two integers LL and RR and outputs the count of integers in the sequence L,L+1,…,R−1,RL,L+1,…,R−1,R whose sum of digits (SOD) is divisible by 33.
+
+// He has developed the user interface (UI) quite easily. He is having a hard time finding the logic to solve the problem. As you are not only a good friend of Ujan but also a good problem solver, he asks you to help him out.
+
+// Can you solve the problem for your friend, Ujan?
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define sec second
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 10000000007
+
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vv;
+
+
+void fastscan(int &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+bool comp(pair<int, int> x, pair<int, int> y)
+{
+    if (x.fi != y.fi)
+        return x.fi > y.fi;
+    else
+        return x.sec > y.sec;
+}
+
+int change(char ch)
+{
+    return ch - '0';
+}
+
+void solve()
+{
+    ll l, r;
+    cin >> l >> r;
+
+    if (l % 3 == 0)l -= (l % 3) + 3;
+    if (r % 3 == 0)r -= (r % 3);
+
+    if (l > r)cout << 0 << endl;
+
+    else
+    {
+        l = l / 3;
+        r = r / 3;
+        cout << (r - l) << endl;
+    }
+}
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
+
+
+
+
+// coded with the ❤️ Avnoor
