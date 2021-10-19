5d41c07a4b50df81cfb2dfa8f2a74d36104e18f8
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2021-10-20 00:03:26 +0530

    Create Lucky_Number.cpp

---
+++ SnackDown 2021 - Online Qualifiers/Lucky_Number.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define sec second
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 10000000007
+
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vv;
+
+
+void fastscan(int &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+bool comp(pair<int, int> x, pair<int, int> y)
+{
+    if (x.fi != y.fi)
+        return x.fi > y.fi;
+    else
+        return x.sec > y.sec;
+}
+
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a == 7 or b == 7 or c == 7)cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        // ll a, b;
+        // cin >> a >> b;
+        // if (solve(a, b))cout << "yes" << endl;
+        // else cout << "no" << endl;
+        solve();
+    }
+    // cout << solve(vec, vec2);
+    return 0;
+}
+
+
+
+
+// coded with the ❤️ Avnoor
