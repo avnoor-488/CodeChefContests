2865a55a343b5b73d7e73f1593755d1594e56501
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2021-10-20 00:05:15 +0530

    Create Maximum Number_Of_Distinct_Elements.cpp

---
+++ SnackDown 2021 - Online Qualifiers/Maximum Number_Of_Distinct_Elements.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define sec second
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 10000000007
+
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vv;
+
+
+void fastscan(int &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+bool comp(pair<int, int> x, pair<int, int> y)
+{
+    // if (x.fi != y.fi)
+    //     return x.fi < y.fi;
+    // else
+    return x.sec < y.sec;
+}
+
+
+
+void solve()
+{
+
+    ll n, x;
+    cin >> n;
+    vector<pair<ll, ll>> vec;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        vec.pb(make_pair(x, i));
+    }
+
+    ll k = 0;
+    sort(vec.begin(), vec.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        if (vec[i].fi > k)
+        {
+            vec[i].fi = k;
+            k++;
+        }
+        else if (vec[i].fi == k)
+        {
+            vec[i].fi = k;
+        }
+    }
+
+    sort(vec.begin(), vec.end(), comp);
+
+    forT(n) {
+        cout << vec[i].fi << " ";
+    }
+
+    cout << endl;
+}
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        // ll a, b;
+        // cin >> a >> b;
+        // if (solve(a, b))cout << "yes" << endl;
+        // else cout << "no" << endl;
+        solve();
+    }
+    // cout << solve(vec, vec2);
+    return 0;
+}
+
+
+
+
+// coded with the ❤️ Avnoor
