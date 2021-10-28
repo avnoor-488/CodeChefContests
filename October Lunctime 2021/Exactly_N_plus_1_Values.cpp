edfd2ae828d786aad247865fb36edc45b6bde3be
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2021-10-28 23:22:36 +0530

    Create Exactly_N_plus_1_Values.cpp

---
+++ October Lunctime 2021/Exactly_N_plus_1_Values.cpp
@@ -0,0 +1,110 @@
+// You are given a positive integer NN. You have to print exactly N+1N+1 positive integers satisfying the following conditions:
+
+// Exactly one value should appear twice, all the remaining values should appear only once.
+// Sum of all these values should be equal to 2N2N.
+// You have to print the values in non-decreasing order. If there are multiple solutions, you can print any of them.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fi first
+#define sec second
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 10000000007
+
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vv;
+
+
+
+void fastscan(int &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+bool comp(pair<int, int> x, pair<int, int> y)
+{
+    // if (x.fi != y.fi)
+    //     return x.fi < y.fi;
+    // else
+    return x.sec < y.sec;
+}
+
+
+
+void solve()
+{
+
+    ll n, x;
+    cin >> n;
+
+    cout << 1 << " ";
+    forT(n)
+    {
+        ll ps = ((ll)1 << i);
+        cout << ps << " ";
+    }
+  
+    cout << endl;
+   
+
+}
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        // ll a, b;
+        // cin >> a >> b;
+        // if (solve(a, b))cout << "yes" << endl;
+        // else cout << "no" << endl;
+        solve();
+    }
+
+
+
+
+    // cout << solve(vec, vec2);
+    return 0;
+}
+
+
+
+
+// coded with the ❤️ Avnoor
