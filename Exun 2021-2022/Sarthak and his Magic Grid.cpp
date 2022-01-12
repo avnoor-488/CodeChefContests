98a4280e967bb360338a3753a0ca8f54c4781480
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2022-01-12 23:44:52 +0530

    Create Sarthak and his Magic Grid.cpp

---
+++ Exun 2021-2022/Sarthak and his Magic Grid.cpp
@@ -0,0 +1,117 @@
+// Sarthak got an empty magic grid of size N×N on his birthday. 
+// The grid can only be filled with positive integers such that the bitwise XOR of all numbers from any row, any column, 
+// or any of the two main diagonals must all be the same! Can you help Sarthak fill this grid using only positive integers no more than 109?
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define sec second
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 1000000007
+// #define num 4 e6 + 5
+
+
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vector<string>> vss;
+// typedef map<int,int>
+
+void fastscan( ll &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+
+
+ll gcd (ll a, ll b)
+{
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+int c2calc(int p,
+           int q)
+{
+    int mod = 998244353, expo;
+    expo = mod - 2;
+    while (expo)
+    {
+        if (expo & 1)
+        {
+            p = (p * q) % mod;
+        }
+        q = (q * q) % mod;
+        expo >>= 1;
+    }
+    return p;
+}
+
+
+void solve()
+{
+    // vi vec;
+    ll n;
+    cin >> n;
+    for (ll i = 1; i <= n; i++)
+    {
+        for (ll j = 1; j <= n; j++)
+        {
+            cout << "1 ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+// coded with the ❤️ Avnoor
+
+
