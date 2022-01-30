1f03dd2bcd95d98ba2687c0158ecfb29dcbcc63e
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2022-01-30 23:52:28 +0530

    Create Permutation XOR Sum.cpp

---
+++ January Lunchtime 2022/Permutation XOR Sum.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define sec second
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 1000000007
+#define all(cont) cont.begin(), cont.end()
+#define vll vector<ll>
+#define pll pair<ll, ll>
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vector<string>> vss;
+
+
+void readVec(vi &vec, int n)
+{
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
+}
+
+
+void printVec(vi vec, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << vec[i] << " ";
+    cout << endl;
+}
+
+void fastscan( ll &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+
+
+ll gcd (ll a, ll b)
+{
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+
+ll helper(ll n) {
+    ll num = 0, count = 0;
+    while (n) {
+        if (n % 2 == 0) num += (1LL << count);
+        n /= 2, count++;
+        if (n <= 0) break;
+    }
+    return num;
+}
+
+void solve()
+{
+    ll n; cin >> n;
+    ll ans = 0;
+    while (n > 0)
+    {
+        ll ai = helper(n);
+        ll bi = n;
+        if ((bi & (bi + 1)) == 0) bi--;
+        ll val = ((bi * (bi + 1)) / 2) - ((ai * (ai - 1)) / 2);
+        ans += (val * 2);
+        n = ai - 1;
+    }
+    cout << ans << endl;
+}
+
+
+
+int main() {
+  
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+// coded with the ❤️ Avnoor
